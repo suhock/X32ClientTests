fdfdf5fdfbcb1180f209a8609d294bab80976e9f
Matthew Suhocki <matt@fivetwo.org>
2022-10-11 14:29:57 +0900

    Isolated X32ClientTests project

--- X32Client/Nodes/AbstractSlotNode.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Suhock.X32.Nodes;
-
-public abstract class AbstractSlotNode : AbstractBaseNode
-{
-    internal AbstractSlotNode(AbstractBaseNode parent, string path) : base(parent, path)
-    {
-    }
-}
